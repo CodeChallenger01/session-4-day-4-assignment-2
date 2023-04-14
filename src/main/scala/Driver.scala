406e6cb27d2c00d333a713c23d78b79153e366d6
Manish Mishra <manish.mishra@knoldus.com>
2023-04-14 21:52:08 +0530

    updated file

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,6 +1,8 @@
+import com.typesafe.scalalogging.Logger
+
 /* This is Driver object that is used to call all the method inside it */
 object Driver extends App {
-
+  val loggers = Logger(getClass)
   /* This 7 line of code is used to print the output of java Calculator class method*/
   private val calculatorTestObj = new CalculatorScala
   private val firstNumber: Int = 100;
@@ -9,25 +11,25 @@ object Driver extends App {
   private val subtraction: Int = calculatorTestObj.subtractTwoElement(firstNumber, secondNumber)
   private val multiply: Int = calculatorTestObj.multiplyTwoElement(firstNumber, secondNumber)
   private val division: Int = calculatorTestObj.divideTwoElement(firstNumber, secondNumber)
-  println("Addition of Two Number :" + addition)
-  println("Subtraction of Two Number :" + subtraction)
-  println("Multiplication of Two Number :" + multiply)
-  println("Division of Two Number :" + division)
+  loggers.info("Addition of Two Number :" + addition)
+  loggers.info("Subtraction of Two Number :" + subtraction)
+  loggers.info("Multiplication of Two Number :" + multiply)
+  loggers.info("Division of Two Number :" + division)
 
   /* The below code is to perform operation on Storage Class methods */
   private val storageObj = new WrappingFromJava
   private val array: Array[Int] = Array(1, 2, 3, 4)
 
   private val arrayAfterAddition: List[Int] = storageObj.addElementInArray(array, array.length, 100)
-  println("\nArray After adding Element : " + arrayAfterAddition)
+  loggers.info("\nArray After adding Element : " + arrayAfterAddition)
 
   private val arrayAfterRemoving: List[Int] = storageObj.removeElementInArray(array, 2)
-  println("Array After removing Element : " + arrayAfterRemoving)
+  loggers.info("Array After removing Element : " + arrayAfterRemoving)
 
   private val checkElementInArray: Boolean = storageObj.checkArrayElement(array, 4)
-  println("Status of number present is array is : " + checkElementInArray)
+  loggers.info("Status of number present is array is : " + checkElementInArray)
 
   private val arrayAfterRemovingAllElement: List[Int] = storageObj.removeAllElement(array)
-  println("After Removing all the element of array : " + arrayAfterRemovingAllElement)
+  loggers.info("After Removing all the element of array : " + arrayAfterRemovingAllElement)
 
 }
